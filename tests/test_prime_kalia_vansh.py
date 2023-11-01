01b4f9f07e2916bd78af75af89cee2186f24f0ef
Vansh Murad Kalia <112732104+chachagsedaro@users.noreply.github.com>
2023-11-01 16:14:53 -0400

    Update test_prime_kalia_vansh.py

--- tests/test_prime_kalia_vansh.py
+++ tests/test_prime_kalia_vansh.py
@@ -1,7 +1,7 @@
 from prime_kalia_vansh import prime_kalia_vansh
 import math
 
-def test_is_prime_param(param=[(2, True), (7, True), (4, False), (10, False), (0, False), (1, False), (-3, False)]):
+def test_is_prime_param(param=[(2, True), (7, True), (4, False), (8, False), (9, False), (10, False), (0, False), (1, False), (-1, False)]):
     for number, is_prime_expected in param:
         is_prime_actual = prime_kalia_vansh.is_prime(number)
         assert is_prime_expected == is_prime_actual, f"Value {number} is not a prime number (Expected: {is_prime_expected}, Actual: {is_prime_actual})"
