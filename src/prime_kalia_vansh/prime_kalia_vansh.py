d5f02889fff2de1d4735a0aa1dab34705099e805
Vansh Murad Kalia <vmk2115@columbia.edu>
2023-11-01 03:15:44 -0400

    third commit

--- src/prime_kalia_vansh/prime_kalia_vansh.py
+++ src/prime_kalia_vansh/prime_kalia_vansh.py
@@ -7,3 +7,24 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+ """
+   Check for Prime Numbers
+
+    This function verifies whether a provided number is a prime number. A prime number is defined as a number greater than 1 that has no divisors other than 1 and itself.
+
+    Parameters:
+    - n (int): The number to be examined.
+
+    Returns:
+    - bool: Returns True if 'n' is a prime number, and False otherwise.
+
+    Example:
+    >>> is_prime(5)
+    True
+
+    >>> is_prime(4)
+    False
+    
+    """
+    
